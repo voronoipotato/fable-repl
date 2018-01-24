ba70e5da2e9de413ea3432d65f01991e0491f294
vorotato <vash.arclan@gmail.com>
2018-01-24 00:08:34 -0500

    added code to get files from gist

--- src/App/util.js
+++ src/App/util.js
@@ -23,6 +23,31 @@ function parseQuery() {
       return params;
     }, {});
 }
+function extractDocumentsFromGist(){
+    var files = JSON.parse(this.response).files;
+    var filterFileList = function(extension){
+      let fileNames =  Object.keys(files)
+      let firstFileName = fileNames.filter(function(x){return x.includes(extension)})[0]
+      let file = files[firstFileName]
+      if(typeof(file) == "undefined"){
+        return ""
+      }else{
+        return file.content.trim()
+      }
+    }
+    //grab the first fsharp file
+    var code = filterFileList('.fs')
+    //grab the first html file
+    var html = filterFileList('.htm')
+    saveState(code, html)
+}
+function getGist(id){
+    var apiUrl = `https://api.github.com/gists/${id}`
+    var r = new XMLHttpRequest();
+    r.onload = extractDocumentsFromGist
+    r.open("GET", apiUrl, true);
+    r.send();
+}
 
 function updateQuery(object) {
     var query = Object.keys(object).map(function(key){
